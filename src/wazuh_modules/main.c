8d83ece4a004a9f3ff0aafbe3b130da43ababb32
arvindkelvin <65662847+arvindkelvin@users.noreply.github.com>
2024-02-25 09:20:02 +0530

    Update main.c

--- src/wazuh_modules/main.c
+++ src/wazuh_modules/main.c
@@ -111,7 +111,7 @@ int main(int argc, char **argv)
 
 void wm_help()
 {
-    print_out("Wazuh Module Manager - %s\nWazuh Inc.", __ossec_version);
+    print_out("thinkSECURITY Module Manager - %s\nWazuh Inc.", __ossec_version);
     print_out(" ");
     print_out("Usage: %s -[d|f|h|t]", ARGV0);
     print_out(" ");
